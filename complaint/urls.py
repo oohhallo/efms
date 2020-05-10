3faf8fe9c186c59be20702ca17a316df4b969153
lokesh <kamineni07@gmail.com>
2020-05-10 15:56:48 +0530

    User Regestration Form created

--- complaint/urls.py
+++ complaint/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('login', views.login_view, name='login'),
+    path('logged-out', views.logging_out_view, name='logout'),
+    path('signup', views.sign_up_view, name='signup')
 ]
--- complaint/views.py
+++ complaint/views.py
@@ -6,22 +6,26 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-
-# Create your views here.
 
 @login_required
 def admin_view_complaints(request):
     complaints = Complaint.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'complaint/complaints_table.html',
-                  context={'complaints': complaints})
+                  context={'complaints': complaints , 'isit_admin': True})
 
 @login_required
 def user_view_complaints(request):
     log_in_user = User.objects.filter(username=request.user.username).first()
     complaints = Complaint.objects.filter(author=log_in_user).order_by('created_date')
     return render(request, 'complaint/complaints_table.html',
-                  context={'complaints': complaints})
+                  context={'complaints': complaints, 'isit_user': True, 'user_name':log_in_user})
+
+@login_required
+def logging_out_view(request):
+    logout(request)
+    return render(request, 'complaint/logout.html')
     
 
 def login_view(request):
@@ -32,7 +36,7 @@ def login_view(request):
         user = authenticate(username=username, password=password)
         if user:
             
-            if is_admin=='on':
+            if is_admin =='on':
                 if user.profile.is_admin:
                     login(request,user)
                     return HttpResponseRedirect(reverse('admin_complaints_view'))
@@ -47,3 +51,12 @@ def login_view(request):
             return render(request, 'complaint/login.html', {'login_fail' :True})
 
     return render(request, 'complaint/login.html')
+
+
+def sign_up_view(request):
+    form=UserCreationForm(request.POST)
+    if form.is_valid():
+        user=form.save()
+        login(request,user)
+        return HttpResponseRedirect(reverse('user_complaints_view'))
+    return render(request, 'complaint/sign_up.html', {'form': form})
