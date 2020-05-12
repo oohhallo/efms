e8c726b37286794d96df60038eaa15b81948be90
lokesh <kamineni07@gmail.com>
2020-05-12 17:36:54 +0530

    adds email field to signup page

--- complaint/forms.py
+++ complaint/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 from .models import Complaint
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 class RegisterComplaintForm(forms.ModelForm):
 
     class Meta:
         model = Complaint
         fields = ('title', 'description', 'category')
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
